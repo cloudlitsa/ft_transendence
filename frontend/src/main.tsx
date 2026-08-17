8fcfc7568375150e00a35a925c8630c20daa4235
Litsa Mouka <evmouka@student.42london.com>
2026-08-17 23:25:38 +0100

    feat: update the alerts list live from the WebSocket

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { ToastProvider } from "./components/ToastProvider.tsx";
 import { AuthProvider } from "./lib/AuthContext.tsx";
+import { AlertsProvider } from "./lib/AlertsContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AuthProvider>
         <ToastProvider>
-          <App />
+          <AlertsProvider>
+            <App />
+          </AlertsProvider>
         </ToastProvider>
       </AuthProvider>
     </BrowserRouter>
